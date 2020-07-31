09b545117dc55c07cd534e773ae24255d3bbc7ba
edvaldotorres <etorres@outlook.com.br>
2020-07-30 21:09:06 -0300

    2 dia

--- src/pages/cadastro/Categorias/index.js
+++ src/pages/cadastro/Categorias/index.js
@@ -2,18 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
-function CadastroCategoria(){
-    return(
-        <PageDefault>
-            <h1>Cadastro de Categoria</h1>
+function CadastroCategoria() {
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria</h1>
+
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
 
 
-
-            <Link to="/">
-                Ir para home
-            </Link>
-        </PageDefault>
-    )
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  )
 }
 
 export default CadastroCategoria;
